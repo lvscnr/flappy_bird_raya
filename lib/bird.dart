cd74696ce560a85b7ce9db92ab429a45918b6ac8
aisyah <75554438+lvscnr@users.noreply.github.com>
2023-04-09 09:26:01 +0800

    Create bird.dart

---
+++ lib/bird.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/matetial.dart';
+
+class MyBIrd extends StatelessWidget{
+  final birdY;
+  final double birdWidth;
+  final double birdHeight;
+
+  MyBird({this.birdY, required this.birdWidth, required this.birdHeight});
+
+  @override
+    Widget build(BuildContext context){
+      return Container {
+      alignment: Alignment (0, (2*birdY + birdHeight)/(2 - birdHeight)),
+      child: Image.asset(
+      'lib/images/bird.png',
+      width: MediaQuery.of(context).size.height * birdWidth / 2,
+      hejght: MediaQuery.of(context).size.height * 3 / 4 * birdHeight / 2,
+      fit: BoxFit.fill,
+      ));
+    }
+  }
+}
